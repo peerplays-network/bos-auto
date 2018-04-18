75335667c7a5b731c3e51d061bfc81e450c3ebe7
Fabian Schuh <Fabian@chainsquad.com>
2018-04-18 15:36:11 +0200

    setup and license

--- setup.py
+++ setup.py
@@ -8,24 +8,24 @@ __VERSION__ = '0.0.3'
 assert sys.version_info[0] == 3, "We require Python > 3"
 
 setup(
-    name='bookied',
+    name='bos-auto',
     version=__VERSION__,
     description=(
         'A daemon to manage synchronization with the blockchain'
     ),
     long_description=open('README.md').read(),
-    download_url='https://github.com/pbsa/bookied/tarball/' + __VERSION__,
-    author='Fabian Schuh',
-    author_email='Fabian.Schuh@BlockchainProjectsBV.com',
+    download_url='https://github.com/pbsa/bos-auto/tarball/' + __VERSION__,
+    author='Blockchain BV',
+    author_email='info@blockchainbv.com',
     maintainer='Fabian Schuh',
     maintainer_email='Fabian.Schuh@BlockchainProjectsBV.com',
     url='http://pbsa.info',
-    keywords=['peerplays', 'bookied'],
+    keywords=['peerplays', 'bos'],
     packages=[
         "bookied",
     ],
     classifiers=[
-        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3',
         'Development Status :: 3 - Alpha',
@@ -33,7 +33,7 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'bookied = bookied.cli:main'
+            'bos-auto = bookied.cli:main'
         ],
     },
     install_requires=[
