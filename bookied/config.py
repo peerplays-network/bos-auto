1b6b4694bde293632e8f095d634132625d7fb11c
Fabian Schuh <Fabian@chainsquad.com>
2018-05-18 10:20:04 +0200

    default config updated

--- bookied/config.py
+++ bookied/config.py
@@ -3,29 +3,105 @@ import yaml
 
 #: Default configuration
 DEFAULT = """
-# (optional) Redis configuration
+######################
+# Witness endpoint
+######################
+# Ideally, the witness endpoint runs on localhost somewhere
+node: ws://localhost:8090
+
+######################
+# PeerPlays network
+######################
+# This allows you to specify which version of bookiesport to take
+# If you connect to Alice (PeerPlays) and try to use network Baxter
+# (PeerPlays testnetwork), an exception will be raised!
+# To clarify
+#
+#   * alice: PeerPlays main blockchain
+#   * baxter: PeerPlays public testnet blockchain
+#   * charlie: PBSA internal development blockchain
+#
+network: baxter
+
+######################
+## Redis configuration
+######################
+# This allows you to optionally use a redis database that is not on
+# localhost.
 redis_host:
 redis_port:
 redis_password:
 redis_db:
 
+#####################
 # Safe mode (if True)
+#####################
+# If this is set to true, the entire software suite will behave as
+# usualy but not send any transactions to the backend node.
 nobroadcast: False
 
-# Wallet passphrase of pypeerplays
+###################
+# Wallet passphrase
+###################
+# Once you added your active private key into local python-peerplays
+# database, you have picked a wallet passphrase. Please provide this
+# here to allow bos-auto to spin up automatically on powerup.
 passphrase: super-secret-wallet-passphrase
 
-# Default proposer account (should be witness account)
+##################
+# Data Proxy IP Whitelist
+##################
+# This option allows to define who is allowed to access the web endpoint
+# to send messages to. This is a list of IP or hostnames that are
+# resovled at the launch time. DNS changes during run time will not
+# propagate!
+#
+#   * 0.0.0.0 - will allow anyone to send messages
+#   * dataproxy.example.com - IP address resolved at startup time.
+#
+api_whitelist:
+ - 0.0.0.0
+ - localhost
+
+##################
+# Proposer account
+##################
+# Please provide the account name of the witnesse that is *proposing*
+# new proposals. This **must** be an active witness account, otherwise,
+# other witnesses will not approve it.
+#
+# Technically, this needs to be the account name of your witness
 BOOKIE_PROPOSER: init0
 
-# Default approving account
+##################
+# Approver account
+##################
+# Provide the account name that approves proposals for witnesses. This
+# must be your active witness-account, otherwise it will not affect
+# consensus of any given proposal for witnesses.
+#
+# Technically, this needs to be the account name of your witness
 BOOKIE_APPROVER: init0
 
+####################################
 # Default mail destination on errors
+####################################
+# if your server is properly set up, you will receive email
+# notifications on errors to this email address
 mailto: info@example.com
 
+###########################
+# Telegram
+###########################
 # Notifications via telgram
+#
+# Please read
+#
+#   https://github.com/sashgorokhov/python-telegram-handler
+#
+# for setting up a telegram notification bot.
 telegram_token:
+#
 # python3 -m telegram_handler "${telegram_token}"
 telegram_chatid:
 """
