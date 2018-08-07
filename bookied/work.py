4de72438de2bad3ba1e6ccdaa1095b2a17c9bfb1
Stefan Schiessl <stefan.schiessl@blockchainprojectsbv.com>
2018-08-07 09:46:10 +0200

    use proper log method
    Signed-off-by: Stefan Schiessl <stefan.schiessl@blockchainprojectsbv.com>

--- bookied/work.py
+++ bookied/work.py
@@ -68,7 +68,7 @@ def process(
     """
     try:
         t = time.time()
-        logging.getLogger(__name__).debug("Processing " + message["unique_string"])
+        log.debug("Processing " + message["unique_string"])
     except Exception as e:
         pass
 
@@ -195,9 +195,10 @@ def process(
 
     try:
         elapsed = time.time() - t
-        logging.getLogger(__name__).debug("Done processing " + message["unique_string"] + ", elapsed time is " + str(elapsed))
+        log.debug("Done processing " + message["unique_string"] + ", elapsed time is " + str(elapsed))
     except Exception as e:
         pass
+
 
 #
 # Approve my own Proposals
